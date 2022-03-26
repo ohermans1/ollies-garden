346ba78a994111f7efeedeb64be70aaabc7348ed
Ollie <ohermans1@gmail.com>
2022-03-26 21:15:54 +1100

    Commit

--- src/app/calculators/calculators.component.ts
+++ src/app/calculators/calculators.component.ts
@@ -3,15 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calculators',
   templateUrl: './calculators.component.html',
-  styleUrls: ['./calculators.component.scss']
+  styleUrls: ['./calculators.component.scss'],
 })
-
-
 export class CalculatorsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    if (!window.location.hash.includes('#ready')) {
+      window.location.href += '#ready';
+      window.location.reload();
+    }
   }
 
+  ngOnInit(): void {}
 }
