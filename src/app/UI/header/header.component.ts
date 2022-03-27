cfc65676eabc2cc37f74608208e93315869735ae
Ollie <ohermans1@gmail.com>
2022-03-27 16:19:15 +1100

    Commit

--- src/app/UI/header/header.component.ts
+++ src/app/UI/header/header.component.ts
@@ -3,18 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    new Promise((resolve) => {
+      this.loadScript();
+    });
   }
   resetButton() {
     const checkbox = document.querySelector(
       '.nav__checkbox'
     ) as HTMLInputElement;
     checkbox.checked = false;
+  }
+
+  public loadScript() {
+    const node = document.createElement('script');
+    node.src = 'assets/calculator.js'; // put there your js file location
+    node.type = 'text/javascript';
+    node.async = true;
+    node.charset = 'utf-8';
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 }
