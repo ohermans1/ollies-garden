ed7109457b8dc85605af7fe531b2a999b06aa2ce
Ollie <ohermans1@gmail.com>
2022-03-27 20:20:32 +1100

    Commit

--- src/app/calculators/calculators.component.ts
+++ src/app/calculators/calculators.component.ts
@@ -8,13 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorsComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    var scriptUrl = 'https://cdn.omnicalculator.com/sdk.js';
-    let node = document.createElement('script');
-    node.src = scriptUrl;
-    node.type = 'text/javascript';
-    node.async = true;
-    node.charset = 'utf-8';
-    document.getElementsByTagName('head')[0].appendChild(node);
-  }
+  ngOnInit(): void {}
 }
